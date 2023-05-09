0d8ff4ebd9fbca249a98f0109217a0f080c33d76
arnold-simanjuntak <arnolddominggo19@gmail.com>
2023-05-09 20:58:32 +0700

    refactor : split into functions

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
-import inquirer from "inquirer";
+import inquirer, { QuestionCollection } from "inquirer";
 import simpleGit from "simple-git";
 import { resolve } from "path";
 import { exec } from "child_process";
 import { createSpinner } from "nanospinner";
 
-const repos = {
+export interface Answers {
+  destinationPath: string;
+  template: string;
+  packageManager: string;
+  autoInstall: boolean;
+}
+
+export const repos = {
   "t3-antd": "https://github.com/dominggo1999/turbo-t3-antd-template",
   t3: "https://github.com/dominggo1999/turbo-next-t3-template",
   "turbo-expo-next": "https://github.com/dominggo1999/turbo-expo-next",
@@ -14,7 +21,7 @@ const repos = {
   "cypress-ts": "https://github.com/dominggo1999/cypress-ts-cucumber-template",
 };
 
-const packageManagers = {
+export const packageManagers = {
   pnpm: "pnpm install",
   yarn: "yarn install",
   npm: "npm install",
@@ -22,86 +29,87 @@ const packageManagers = {
   none: "",
 };
 
-// Prompt the user for the destination path and template choice
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "destinationPath",
-      message: "Enter the destination path:",
-      default: ".",
-    },
-    {
-      type: "list",
-      name: "template",
-      message: "Choose a repository template to clone:",
-      choices: Object.keys(repos),
-    },
-    {
-      type: "list",
-      name: "packageManager",
-      message: "Choose a package manager to install dependencies:",
-      choices: Object.keys(packageManagers),
-      default: "pnpm",
-    },
-    {
-      type: "confirm",
-      name: "autoInstall",
-      message: "Automatically install dependencies?",
-      default: true,
-    },
-  ])
-  .then((answers) => {
-    const { destinationPath, template, packageManager, autoInstall } =
-      answers as {
-        destinationPath: string;
-        template: string;
-        packageManager: string;
-        autoInstall: boolean;
-      };
+export const prompts: QuestionCollection = [
+  {
+    type: "input",
+    name: "destinationPath",
+    message: "Enter the destination path:",
+    default: ".",
+  },
+  {
+    type: "list",
+    name: "template",
+    message: "Choose a repository template to clone:",
+    choices: Object.keys(repos),
+  },
+  {
+    type: "list",
+    name: "packageManager",
+    message: "Choose a package manager to install dependencies:",
+    choices: Object.keys(packageManagers),
+    default: "pnpm",
+  },
+  {
+    type: "confirm",
+    name: "autoInstall",
+    message: "Automatically install dependencies?",
+    default: true,
+  },
+];
 
-    // Get the URL of the selected template
-    const url = repos[template];
-    const resolvedPath = resolve(process.cwd(), destinationPath);
+export const cloneRepository = async (answers: Answers) => {
+  const { destinationPath, template, packageManager, autoInstall } = answers;
 
-    const sClone = createSpinner("Cloning the repository").start();
+  // Get the URL of the selected template
+  const url = repos[template];
+  const resolvedPath = resolve(process.cwd(), destinationPath);
 
-    // Clone the repository
-    simpleGit().clone(url, resolvedPath, null, (error) => {
-      if (error) {
-        sClone.error();
-        console.error("Error occurred while cloning the repository:", error);
-      } else {
-        sClone.success();
-        console.log("Repository cloned successfully!");
+  const sClone = createSpinner("Cloning the repository").start();
 
-        // If autoInstall is true, install dependencies
-        if (autoInstall && packageManager !== "none") {
-          const sInstall = createSpinner(
-            "Installing all the dependencies",
-          ).start();
+  try {
+    await simpleGit().clone(url, resolvedPath);
+    sClone.success();
+    console.log("Repository cloned successfully!");
 
-          // cd into the cloned repository
-          process.chdir(resolvedPath);
+    // If autoInstall is true, install dependencies
+    if (autoInstall && packageManager !== "none") {
+      await installDependencies(resolvedPath, packageManager);
+    }
+  } catch (error) {
+    sClone.error();
+    console.error("Error occurred while cloning the repository:", error);
+  }
+};
 
-          const packageManagerCommand = packageManagers[packageManager];
+export const installDependencies = (
+  resolvedPath: string,
+  packageManager: string,
+) => {
+  const sInstall = createSpinner("Installing all the dependencies").start();
 
-          exec(packageManagerCommand, (error) => {
-            if (error) {
-              sInstall.error();
-              console.error(
-                "Error occurred while installing dependencies:",
-                error,
-              );
-            } else {
-              sInstall.success();
-              console.log("Dependencies installed successfully!");
-            }
-          });
-        }
-      }
-    });
-  })
-  .catch((error) => {
-    console.error("An error occurred:", error);
+  // cd into the cloned repository
+  process.chdir(resolvedPath);
+
+  const packageManagerCommand = packageManagers[packageManager];
+
+  exec(packageManagerCommand, (error) => {
+    if (error) {
+      sInstall.error();
+      console.error("Error occurred while installing dependencies:", error);
+    } else {
+      sInstall.success();
+      console.log("Dependencies installed successfully!");
+    }
   });
+};
+
+export const promptUser = async (prompts: QuestionCollection) => {
+  try {
+    const answers = await inquirer.prompt(prompts);
+    await cloneRepository(answers as Answers);
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+};
+
+promptUser(prompts);
